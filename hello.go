4355524bdaaee4608768e3a55c4d183f3a8e13bb
KEvin Wang <kvn.a.wng@gmail.com>
2014-06-30 04:27:02 -0700

    more go practice
    learning go by doing

--- hello.go
+++ hello.go
@@ -19,9 +19,29 @@ func practice(){
 	y := 4
 	sum, prod := addAndMult(x,y)
 	fmt.Println("sum:", sum, "prod:", prod)
+	learnTypes()
 
 }
 
 func addAndMult(x, y int) (sum, prod int){
 	return x + y, x * y
+}
+
+func learnTypes(){
+	s := "Learn Go!"
+	s2 := `a "raw" string literal
+	can include line breaks.`
+
+	f := 3.14159
+	c := 3 + 4i
+
+	//var syntax with initializers.
+	var u uint = 7
+	var pi float32 = 22. / 7
+
+	n := byte('\n')
+
+	//arrays have size fixed at compile time.
+	var a4 [4]int
+	a3 := [...]int{3, 1, 5}
 }
