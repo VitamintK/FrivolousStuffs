f79ee57f119eaf8c3c13948826036a8da6af8e0c
KEvin Wang <kvn.a.wng@gmail.com>
2014-07-01 04:20:15 -0700

    Golang practice with Proj Euler

---
+++ euler17.go
@@ -0,0 +1,17 @@
+package main
+import "fmt"
+
+func main(){
+	Fmt.Printf(euler())
+}
+
+func euler() (endamount int){
+	ones := [...]string{"","one","two","three","four","five","six","seven","eight","nine"}
+	tens := [...]string{"","","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"}
+	//hundred = 7, and = 3
+
+}
+
+func numtostr(num int) (str string){
+	
+}
