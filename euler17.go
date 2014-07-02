70d3d5a623145f4b0c184c9363e099a2d4f87635
KEvin Wang <kvn.a.wng@gmail.com>
2014-07-02 03:37:38 -0700

    golang practice with proj euler
    funcion numtostring takes a number from 1 to 1000 and returns a string
    of the number in english.

--- euler17.go
+++ euler17.go
@@ -1,17 +1,31 @@
 package main
+
 import "fmt"
 
 func main(){
-	Fmt.Printf(euler())
+	fmt.Println(euler())
 }
 
 func euler() (endamount int){
-	ones := [...]string{"","one","two","three","four","five","six","seven","eight","nine"}
-	tens := [...]string{"","","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"}
-	//hundred = 7, and = 3
-
+	fmt.Println(numtostr(443))
+	endamount = 0
+	return
 }
 
 func numtostr(num int) (str string){
-	
+	ones := [...]string{"","one","two","three","four","five","six","seven","eight","nine"}
+	tens := [...]string{"","","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"}
+	//hundred = 7, and = 3
+	if num>1000{
+		str = ""
+	} else {
+		one,ten,hundred,thousand := num%10, (num%100)/10, (num%1000)/100, num/1000
+		fmt.Println(one,ten,hundred,thousand)
+		//str = ones[hundred] + " hundred and " + tens[ten] + " " + ones[one]
+		lengthof = len(ones[hundred]) + len(tens[ten]) + len(ones[one]) //+conditional length of "hundred and" if hundred exists
+		str = ""
+		fmt.Println(str)
+	}
+	return
+
 }
