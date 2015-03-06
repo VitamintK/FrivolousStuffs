eef62929da00f576abe365ffb6046f4514c47509
KEvin Wang <kevinwang@kevinwang.us>
2015-03-06 00:07:13 -0800

    GAUSSIAN!!!!

--- birthproblem.py
+++ birthproblem.py
@@ -4,22 +4,38 @@ If they have a boy, they have another child.
 If they have a girl, they stop.
 What is the proportion of boys to girls in the country?"""
 import random
+import statistics
 
-FAMILIES = 1000 #amount of trials.
+TRIALS = 500 #amount of trials
+FAMILIES = 500 #amount of families per trial.
 
 def birth():
     """True = girl.  False = boy"""
     return random.random() > 0.5
 
-all_children = []
-for family in range(FAMILIES):
-    children = []
-    while True:
-        child = birth()
-        children.append(child)
-        if child:
-            break
-    all_children.extend(children)
+def run_trial(families):
+    all_children = []
+    for family in range(FAMILIES):
+        children = []
+        while True:
+            child = birth()
+            children.append(child)
+            if child:
+                break
+        all_children.extend(children)
+    #print("girls: {}, boys: {}, percentage of girls of all children: {}".format(
+    #    all_children.count(True), all_children.count(False), all_children.count(True)/len(all_children)))
+    return all_children.count(True), all_children.count(False)
 
-print("girls: {}, boys: {}, percentage of girls of all children: {}".format(
-    all_children.count(True), all_children.count(False), all_children.count(True)/len(all_children)))
+def run_trials(trials, families):
+    results = []
+    for trial in range(trials):
+        trial_result = run_trial(families)
+        results.append(trial_result)
+    return results
+
+results = run_trials(TRIALS, FAMILIES)
+avg_pct_girl = statistics.mean(map(lambda x: x[0]/(x[0] + x[1]), results))
+
+print("""Out of {} trials of {} families each, the average percentage of girls
+that makes up each population is {}""".format(TRIALS, FAMILIES, avg_pct_girl))
