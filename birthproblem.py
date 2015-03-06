f6e79567649e7a084d2504845d3395c25de9c1d2
KEvin Wang <kevinwang@kevinwang.us>
2015-03-06 00:39:20 -0800

    interesting results w/sample size

--- birthproblem.py
+++ birthproblem.py
@@ -13,9 +13,9 @@ def birth():
     """True = girl.  False = boy"""
     return random.random() > 0.5
 
-def run_trial(families):
+def run_trial(families) -> ('girls', 'boys'):
     all_children = []
-    for family in range(FAMILIES):
+    for family in range(families):
         children = []
         while True:
             child = birth()
@@ -23,19 +23,22 @@ def run_trial(families):
             if child:
                 break
         all_children.extend(children)
-    #print("girls: {}, boys: {}, percentage of girls of all children: {}".format(
-    #    all_children.count(True), all_children.count(False), all_children.count(True)/len(all_children)))
     return all_children.count(True), all_children.count(False)
 
-def run_trials(trials, families):
+def run_trials(trials, families) -> [('girls', 'boys')]:
     results = []
     for trial in range(trials):
         trial_result = run_trial(families)
         results.append(trial_result)
     return results
 
-results = run_trials(TRIALS, FAMILIES)
-avg_pct_girl = statistics.mean(map(lambda x: x[0]/(x[0] + x[1]), results))
+def get_percent_girl(trials, families):
+    results = run_trials(trials, families)
+    avg_pct_girl = statistics.mean(map(lambda x: x[0]/(x[0] + x[1]), results))
+    print("""Out of {} trials of {} families each, the average percentage of girls
+that makes up each population is {}""".format(trials, families, avg_pct_girl))
+    return avg_pct_girl
 
-print("""Out of {} trials of {} families each, the average percentage of girls
-that makes up each population is {}""".format(TRIALS, FAMILIES, avg_pct_girl))
+get_percent_girl(TRIALS,FAMILIES)
+get_percent_girl(1, 500)
+get_percent_girl(500,1)
