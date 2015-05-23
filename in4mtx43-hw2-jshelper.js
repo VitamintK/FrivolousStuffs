ea2571844ee0e2b7b37015210d9d35c8994cd5be
KEvin Wang <kevinwang@kevinwang.us>
2015-05-22 21:39:29 -0700

    inf4mtx js helper

---
+++ in4mtx43-hw2-jshelper.js
@@ -0,0 +1,13 @@
+var Professional = ["Name", "Email", "latitude", "longitude", "date1", "time1", "date2", "time2", "English", "Spanish", "Chinese", "promotional factor"];
+
+var change_to_default = function(languages){
+	$("#name").val("Kevin");
+	$("#email").val("Kevin@kevin.com");
+	$("#latitude").val("1");
+	$("#longitude").val("1");
+	$("#dateOne").val("05/22/2015");
+	$("#dateTwo").val("05/22/2015");
+	$("#pf").val("1");
+};
+
+change_to_default('');
--- learningMEAN/myapp/app.js
+++ learningMEAN/myapp/app.js
@@ -1,16 +1,38 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
+//We aren't specifying a route here, so it will apply to all of them
+app.use(function(req, res, next) {
+    console.log('Time:', Date.now(), req.url);
+    next();
+});
+
+//We'll make an Array to save the words in
+var wordHistory = []; 
+//This middleware will only apply to the one we care about
+app.use('/say/:word', function(req, res, next) {
+    wordHistory.push(req.params.word);
+    console.log("Word History:", wordHistory);
+    next();
+});
+
+//Put this above your server declaration!
+app.use(function (req, res) {
+  res.status(404).send('Nothing to see here!');
+});
+
+app.route('/').get(function (req, res) {
   res.send('Hello World!');
 });
 
-var server = app.listen(1337, function () {
+app.route('/say/:word').get(function (req,res) {
+  user_word = req.params.word;
+  res.send('You said: ' + user_word);
+});
 
+var server = app.listen(1337, function () {
   var host = server.address().address;
   var port = server.address().port;
-
   console.log('Example app listening at http://%s:%s', host, port);
-
 });
 
